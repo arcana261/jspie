e59c253af277b9c02e89795b1d6bf2c3843861aa
Mohamad mehdi Kharatizadeh <m_kharatizadeh@yahoo.com>
2016-09-24 15:15:49 +0330

    added literal for acceptLanguage to headers module

--- tests/http/headers.js
+++ tests/http/headers.js
@@ -3,12 +3,12 @@ const expect = require('chai').expect;
 
 describe('JsPieHttpHeaders', () => {
   describe('#literals', () => {
-    it('should be frozen', () => {
-      expect(headers).to.be.frozen;
-    });
-
-    it('Content-Type', () => {
-      expect(headers.contentType).to.be.equal('content-type');
-    });
+    it('should be frozen', () => expect(headers).to.be.frozen);
+    it('Content-Type', () =>
+      expect(headers.contentType).to.be.equal('content-type'));
+    it('Authorization', () =>
+      expect(headers.authorization).to.be.equal('authorization'));
+    it('Accept-Langauge', () =>
+      expect(headers.acceptLanguage).to.be.equal('accept-language'));
   });
 });
